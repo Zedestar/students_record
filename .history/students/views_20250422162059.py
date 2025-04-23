2cf88514d57839922428e43f3733f84962c0b0f8
Melkizedeki <mmagota2@gmail.com>
2025-04-23 08:15:36 +0300

    The commit after adding the restframework

--- .history/students/views_20250422162059.py
+++ .history/students/views_20250422162059.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Student, Course
-from .serializers import StudentListSerializer, CourseListSerializer
 
 # Create your views here.
 
